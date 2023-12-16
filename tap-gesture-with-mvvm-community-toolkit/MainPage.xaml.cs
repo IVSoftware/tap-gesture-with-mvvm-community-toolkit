dd3b59e0d77738450ffc4b9b45bddfe858476f73
Tom Gregor <tgregor@ivsoftware.com>
2023-12-16 06:50:10 -0700

    works

--- tap-gesture-with-mvvm-community-toolkit/MainPage.xaml.cs
+++ tap-gesture-with-mvvm-community-toolkit/MainPage.xaml.cs
@@ -1,19 +1,36 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace tap_gesture_with_mvvm_community_toolkit
 {
     public partial class MainPage : ContentPage
     {
-        public MainPage() => InitializeComponent();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext.LoadingListCollection.Add("Apple");
+            BindingContext.LoadingListCollection.Add("Orange");
+            BindingContext.LoadingListCollection.Add("Banana");
+        }
+        new Model.SearchLoadingListVM BindingContext => (Model.SearchLoadingListVM)base.BindingContext;
     }
     // https://learn.microsoft.com/en-us/dotnet/communitytoolkit/mvvm/relaycommand#working-with-icommand
-    partial class SearchLoadingListVM : ObservableObject
+
+    namespace Model
     {
-        [RelayCommand]
-        private async Task CounterButtonClick(object param)
+        partial class SearchLoadingListVM : ObservableObject
         {
-            await Task.Delay(1000);
+            [RelayCommand]
+            private async Task ItemTaped(string s)
+            {
+                if (App.Current?.MainPage is Page main)
+                {
+                    await main.DisplayAlert("Alert", $"{s} was tapped.", "OK");
+                }
+            }
+            public ObservableCollection<string> LoadingListCollection { get; } = new ObservableCollection<string>();
         }
     }
 }
